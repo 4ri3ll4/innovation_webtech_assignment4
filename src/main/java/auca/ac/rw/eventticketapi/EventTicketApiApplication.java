26d9efd626ef55f2e3b4bb7fb1d8f38dabf3e0de
4ri3ll4 <ariellaa720@gmail.com>
2026-09-19 19:59:40 +0200

    adding event ticket api application

---
+++ src/main/java/auca/ac/rw/eventticketapi/EventTicketApiApplication.java
@@ -0,0 +1,11 @@
+package auca.ac.rw.eventticketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventTicketApiApplication.class, args);
+    }
+}
